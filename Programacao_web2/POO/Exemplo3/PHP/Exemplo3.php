e4dc4d8736b7c55e1eb78c887e21623e9190f0df
leonardo <Leonardo.f.damasio@gmail.com>
2026-03-02 15:39:21 -0300

    Update

--- Programacao_web2/POO/Exemplo3/PHP/Exemplo3.php
+++ Programacao_web2/POO/Exemplo3/PHP/Exemplo3.php
@@ -15,9 +15,21 @@
 
     require "Exemplo3.inc.php";
 
-    $conta1 = new ContaBancaria("Matheus Roberto");
+    $conta1 = new ContaBancaria(5000);
+    $conta2 = new ContaBancaria(10000);
 
+    //Despositar
+    $conta1->depositar(1000);
+    $conta2->depositar(1000);
 
+    //Sacar
+    $conta1->sacar(1000);
+
+    //Acessar saldo
+    $saldo1 = $conta1->getSaldo();
+    $saldo2 = $conta2->getSaldo();
+
+    echo "<p>Após as operações de saque e depósito, seu saldo atual, para ambas as contas, é o sequinte: <br> CONTA_1 = R$$saldo1 <br> CONTA_2 = R$$saldo2</p>"
 ?>
 
 </body>
